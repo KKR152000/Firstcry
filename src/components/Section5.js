e94cb78446ca55692974b2c022e0d79d34f96387
KKR152000 <79276300+KKR152000@users.noreply.github.com>
2021-03-19 22:46:43 +0530

    Add files via upload

---
+++ src/components/Section5.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Section5() {
+    return (
+            <div id="sec5-card">
+                <h4 className="sec5-h4">Spring Is Here</h4>
+                <div className="row">
+                        <div className=" col-lg-6 col-md-6">
+                            <a href="/"><img src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/615_384_shop_him_120221_01.jpg" alt="..."/></a>
+                        </div>
+                        <div className=" col-lg-6 col-md-6">
+                            <a href="/"><img src="https://cdn.fcglcdn.com/brainbees/images/cattemplate/615_384_shop_her_120221_02.jpg" alt="..."/></a>
+                        </div>
+                </div>
+             </div>
+    )
+}
+
+export default Section5
